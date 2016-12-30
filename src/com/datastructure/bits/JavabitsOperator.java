44dd386b39b498be1b6354baa9fd7d98ea3aeec2
ghanshyamgupta07 <ghanshyam_cs@hotmail.com>
2016-12-30 20:44:39 +0530

    Update JavabitsOperator.java

--- src/com/datastructure/bits/JavabitsOperator.java
+++ src/com/datastructure/bits/JavabitsOperator.java
@@ -2,10 +2,10 @@ package com.datastructure.bits;
 
 public class JavabitsOperator {
 
-	public static void main(String[] args) {
+   public static void main(String[] args) {
 
-     int num1 = 32 ; // rep 00100000
-     int num2 = 13 ; // rep 00001101
+     int num1 = 32 ; // represents 00100000
+     int num2 = 13 ; // represents 00001101
 
      System.out.println(num2^num1);
      
